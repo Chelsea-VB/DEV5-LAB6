7f8aa42391ffba061bd9da75a0cfc6e46713ae20
Chelsea-VB <vanbreda.chelsea@skynet.be>
2022-11-16 13:36:41 +0100

    feat: add structure

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,22 +1,35 @@
 <script setup>
-import { onMounted, ref } from 'vue';
+import { onMounted, reactive, ref } from 'vue';
+let comment = reactive({ comment: []});
 
 onMounted(() => {
   const api_url = "https://lab5-p379.onrender.com/api/v1/messages/";
   fetch(api_url)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
+        console.log(data);
+        comment.comment = data;
+
     });
 });
 
 </script>
 
 <template>
-    <div class="chat">
-
+    <div class="comments">
+        <div class="comments__header">
+          <h3>Comments</h3>
+          <div class="comment">
+            <ul>
+              <li>
+                <h4>Username</h4>
+                <p>Comment</p>
+              </li>
+            </ul>
+          </div>
+        </div>
     </div>
-</template>
+  </template>
 
 <style scoped>
 
